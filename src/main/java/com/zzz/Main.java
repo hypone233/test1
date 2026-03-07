111df97b62cddfb7ec233ba19fb4fa122fd911ed
hypone233 <19213253165@163.com>
2026-03-07 22:48:04 +0800

    滑动窗口

--- src/main/java/com/zzz/Main.java
+++ src/main/java/com/zzz/Main.java
@@ -78,5 +78,21 @@ public class Main {
         System.out.println(q.poll());
         if(!q.isEmpty()) System.out.println("meikong");*/
 
+
     }
 }
+/*
+class Solution {
+    public int tribonacci(int n) {
+        int[] dp = new int[n+1];
+        int i=3;
+        dp[0] = 0;
+        if(n>=1)dp[1] = 1;
+        if(n>=2)dp[2] = 1;
+        while(i <= n){
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            i++;
+        }
+        return dp[n];
+    }
+}*/
