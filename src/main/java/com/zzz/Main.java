045567c63bd848f6b5a2e92225a1f4b2946b04bb
hypone233 <19213253165@163.com>
2026-03-08 13:54:39 +0800

    BC数字统计，按位查找，结果不如遍历感觉是n太小

--- src/main/java/com/zzz/Main.java
+++ src/main/java/com/zzz/Main.java
@@ -1,6 +1,5 @@
 package com.zzz;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
@@ -120,3 +119,79 @@ class Solution {
     }
 }
 */
+//nk,BC数字统计
+/*
+import java.util.Scanner;
+import java.util.*;
+        import java.io.*;
+
+public class Main {
+    public static PrintWriter out = new PrintWriter(new BufferedWriter(
+            new OutputStreamWriter(System.out)));
+    public static Read in = new Read();
+
+    public static void main(String[] args) throws IOException{
+
+        int left = in.nextInt();
+        int right = in.nextInt();
+
+        // 使用数位统计方法：count(right) - count(left-1)
+        int res = countDigitTwo(right) - countDigitTwo(left - 1);
+
+        out.print(res);
+        out.close();
+    }
+
+    // 统计1到n中数字2出现的次数
+    private static int countDigitTwo(int n) {
+        if (n <= 0) return 0;
+
+        int count = 0;
+        int factor = 1;  // 位权：1, 10, 100, ...
+
+        while (n / factor > 0) {
+            int higher = n / (factor * 10);  // 高位部分
+            int current = (n / factor) % 10; // 当前位
+            int lower = n % factor;           // 低位部分
+
+            if (current < 2) {
+                count += higher * factor;
+            } else if (current == 2) {
+                count += higher * factor + lower + 1;
+            } else { // current > 2
+                count += (higher + 1) * factor;
+            }
+
+            factor *= 10;
+        }
+
+        return count;
+    }
+}
+
+class Read { // 自定义快速读入
+    StringTokenizer st = new StringTokenizer("");
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    String next() throws IOException {
+        while (!st.hasMoreTokens()) {
+            st = new StringTokenizer(bf.readLine());
+        }
+        return st.nextToken();
+    }
+
+    String nextLine() throws IOException {
+        return bf.readLine();
+    }
+
+    int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+}*/
