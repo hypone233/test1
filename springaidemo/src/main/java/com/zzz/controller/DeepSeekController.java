dc1fae1cbdfe539186d1bdbe88d02d7f4ac316cd
hypone233 <19213253165@163.com>
2026-03-18 22:21:01 +0800

    接入ds

--- springaidemo/src/main/java/com/zzz/controller/DeepSeekController.java
+++ springaidemo/src/main/java/com/zzz/controller/DeepSeekController.java
@@ -1,4 +1,23 @@
 package com.zzz.controller;
 
+
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/ds")
+@RestController
 public class DeepSeekController {
+
+    @Autowired
+    private OpenAiChatModel openAiChatModel;
+
+    @RequestMapping("/chat")
+    public String generate(String message){
+        return openAiChatModel.call(message);
+    }
+
 }
